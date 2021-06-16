ebe493429ce8fbaede1daecc8a15c4c82a709754
peternabil <peternabil@github.com>
2021-06-16 07:19:32 +0200

    added the yolo and genderage models

---
+++ people_counter/gender_age/get_gender_age.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+import cv2
+import dlib
+import numpy as np
+import argparse
+from contextlib import contextmanager
+from omegaconf import OmegaConf
+from tensorflow.keras.utils import get_file
+from gender_age.src.factory import get_model
+import PIL
+
+
+def main_driver(img,model,detector,img_size):
+    # image_generator = yield_images_from_dir(image_dir) if image_dir else yield_images()
+    # print(model.summary())
+    margin = 0
+    predictions = []
+    # for img in imgs:
+    input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # print(input_img.shape)
+    img_h, img_w, _ = input_img.shape
+    
+    # detect faces using dlib detector
+    detected = detector(input_img, 1)
+    # print(detected)
+    faces = np.empty((len(detected), img_size, img_size, 3))
+    
+    if len(detected) > 0:
+        for i, d in enumerate(detected):
+            x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
+            xw1 = max(int(x1 - margin * w), 0)
+            yw1 = max(int(y1 - margin * h), 0)
+            xw2 = min(int(x2 + margin * w), img_w - 1)
+            yw2 = min(int(y2 + margin * h), img_h - 1)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
+            faces[i] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1], (img_size, img_size))
+
+        # predict ages and genders of the detected faces
+        results = model.predict(faces)
+        predicted_genders = results[0]
+        ages = np.arange(0, 101).reshape(101, 1)
+        predicted_ages = results[1].dot(ages).flatten()
+
+        # draw results
+        for i, d in enumerate(detected):
+            label = "{}, {}".format(int(predicted_ages[i]),"M" if predicted_genders[i][0] < 0.5 else "F")
+            predictions.append([int(predicted_ages[i]),"M" if predicted_genders[i][0] < 0.5 else "F"])
+
+    return predictions
+
+def detect_cropped_imgs(persons,img_size,model):
+    predictions = []
+    for person in persons:
+        person_img = cv2.imread('persons-imgs/'+person)
+        # print(person_img.shape)
+        person_img = cv2.resize(person_img,(img_size,img_size))
+        use_arr = np.array([person_img])
+        # print(use_arr.shape)
+        
+        pred = model.predict(use_arr)
+
+        predicted_gender = pred[0]
+        ages = np.arange(0, 101).reshape(101, 1)
+        predicted_age = pred[1].dot(ages).flatten()
+        # label = "{}, {}".format(int(predicted_age[0]),"M" if predicted_gender[0][0] < 0.5 else "F")
+        predictions.append([int(predicted_age[0]),"M" if predicted_gender[0][0] < 0.5 else "F"])
+    return predictions
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     weight_file = get_file("EfficientNetB3_224_weights.11-3.44.hdf5", pretrained_model, cache_subdir="pretrained_models",file_hash=modhash, cache_dir=str(Path(__file__).resolve().parent))
+
+#     # for face detection
+#     # detector = dlib.get_frontal_face_detector()
+
+#     # load model and weights
+#     model_name, img_size = Path(weight_file).stem.split("_")[:2]
+#     img_size = int(img_size)
+#     cfg = OmegaConf.from_dotlist([f"model.model_name={model_name}", f"model.img_size={img_size}"])
+#     model = get_model(cfg)
+#     model.load_weights(weight_file)
+#     imgs = [cv2.imread('imgs/img1.jpg'),cv2.imread('imgs/img2.jpg'),cv2.imread('imgs/img3.jpg')]
+#     print(main_driver(imgs,model,detector))
---
+++ people_counter/object_detection/utils.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+import time
+def non_max_suppression(inputs, model_size, max_output_size,
+                        max_output_size_per_class, iou_threshold,
+                        confidence_threshold):
+    bbox, confs, class_probs = tf.split(inputs, [4, 1, -1], axis=-1)
+    bbox=bbox/model_size[0]
+    scores = confs * class_probs
+    boxes, scores, classes, valid_detections = \
+        tf.image.combined_non_max_suppression(
+        boxes=tf.reshape(bbox, (tf.shape(bbox)[0], -1, 1, 4)),
+        scores=tf.reshape(scores, (tf.shape(scores)[0], -1,
+                                   tf.shape(scores)[-1])),
+        max_output_size_per_class=max_output_size_per_class,
+        max_total_size=max_output_size,
+        iou_threshold=iou_threshold,
+        score_threshold=confidence_threshold
+    )
+    return boxes, scores, classes, valid_detections
+def resize_image(inputs, modelsize):
+    inputs= tf.image.resize(inputs, modelsize)
+    return inputs
+def load_class_names(file_name):
+    with open(file_name, 'r') as f:
+        class_names = f.read().splitlines()
+    return class_names
+def output_boxes(inputs,model_size, max_output_size, max_output_size_per_class,
+                 iou_threshold, confidence_threshold):
+    center_x, center_y, width, height, confidence, classes = \
+        tf.split(inputs, [1, 1, 1, 1, 1, -1], axis=-1)
+    top_left_x = center_x - width / 2.0
+    top_left_y = center_y - height / 2.0
+    bottom_right_x = center_x + width / 2.0
+    bottom_right_y = center_y + height / 2.0
+    inputs = tf.concat([top_left_x, top_left_y, bottom_right_x,
+                        bottom_right_y, confidence, classes], axis=-1)
+    boxes_dicts = non_max_suppression(inputs, model_size, max_output_size,
+                                      max_output_size_per_class, iou_threshold, confidence_threshold)
+    return boxes_dicts
+
+    #image, boxes, scores, classes, nums, class_names
+def draw_outputs(img, boxes, objectness, classes, nums, class_names):
+    boxes, objectness, classes, nums = boxes[0], objectness[0], classes[0], nums[0]
+    boxes=np.array(boxes)
+    person_num = 0
+    for i in range(nums):
+        x1y1 = tuple((boxes[i,0:2] * [img.shape[1],img.shape[0]]).astype(np.int32))
+        x2y2 = tuple((boxes[i,2:4] * [img.shape[1],img.shape[0]]).astype(np.int32))
+        # crop_img = img[x1y1[1]:x2y2[1], x1y1[0]:x2y2[0]]
+        # cv2.imwrite("Object"+str(i)+".jpg",crop_img)
+        img = cv2.rectangle(img, (x1y1), (x2y2), (255,0,0), 2)
+        img = cv2.putText(img, '{} {:.4f}'.format(
+            class_names[int(classes[i])], objectness[i]),
+                          (x1y1), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+        if int(classes[i]) == 0:
+            person_num+=1
+    return img,person_num
+
+def crop_persons(img, boxes, objectness, classes, nums):
+    boxes, objectness, classes, nums = boxes[0], objectness[0], classes[0], nums[0]
+    boxes=np.array(boxes)
+    person_num = 0
+    persons = []
+    for i in range(nums):
+        if int(classes[i]) == 0:
+            img = img
+            x1y1 = tuple((boxes[i,0:2] * [img.shape[1],img.shape[0]]).astype(np.int32))
+            x2y2 = tuple((boxes[i,2:4] * [img.shape[1],img.shape[0]]).astype(np.int32))
+            # crop_img = img
+            crop_img = img[x1y1[1]:x2y2[1], x1y1[0]:x2y2[0]]
+            # crop_img = crop_img.reshape(crop_img.shape[1],crop_img.shape[2],crop_img.shape[3])
+            # print("the img")
+            # print(crop_img.shape)
+            name = "Object"+str(i)+".jpg"
+            persons.append(name)
+            cv2.imwrite('persons-imgs/'+name,crop_img)
+    return persons
+
